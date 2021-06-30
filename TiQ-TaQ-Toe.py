464c26a9c4611249dfdf48147da4470e032a4c4c
Rijul Sachdeva <71190505+Rij29@users.noreply.github.com>
2021-07-01 02:55:17 +0530

    Update TiQ-TaQ-Toe.py

--- TiQ-TaQ-Toe.py
+++ TiQ-TaQ-Toe.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 22 18:03:56 2021
+
+@author: rijul
+"""
+
 import numpy as np
 from itertools import product
 import random
@@ -6,15 +14,14 @@ import functools
 import operator
 
 def initial_board():
-  '''This is the initial board with amplitude 1 and 0s in all the places'''
-  
+    """This is the initial board with amplitude 1 and 0s in all the places"""
     initial_board = [[1,'000000000']]
     return initial_board
     
 def amplitudes():
-  '''This returns a list of valid combinations of amplitudes 
-      Eg. [[1,0],[0,1],[-1,0].....]''''
-  
+    """This returns a list of valid combinations of amplitudes 
+      Eg. [[1,0],[0,1],[-1,0].....]"""
+      
     all_amplitudes = [0, 1, -1 , 1j, -1j, 1/np.sqrt(2), -1/np.sqrt(2), 1j/np.sqrt(2), -1j/np.sqrt(2)]
 
     perm = list(product(all_amplitudes,repeat=2))
@@ -27,34 +34,31 @@ def amplitudes():
     
 
 def action(one_board):
-  '''This funnction returns the randomly chosen amplitudes and positions that is to be replaced.
+    """This funnction returns the randomly chosen amplitudes and positions that is to be replaced.
       Eg. amp = [0,1], chosen_zeros = [3,7]
       
       one_board: list (It is a single board from all the possible boards in the list of boards)
-                 Eg. [1,'121221112']'''
+                 Eg. [1,'121221112']"""
   
     amplitude_list = amplitudes()
     amp = random.choice(amplitude_list)
     zeros = [k for k in range(len(one_board)) if one_board.startswith('0', k)]
-    #print(zeros)
     if (len(zeros)>= 2):
         chosen_zeros = random.sample(set(zeros), 2)
-        return amp, chosen_zeros  #This returns the amplitudes and positions chosen
+        return amp, chosen_zeros
     elif (len(zeros)==1):
         chosen_zeros = random.sample(set(zeros), 1)
-        return chosen_zeros       #This returns only the positions since in the last move amplitudes are not important
+        return chosen_zeros
     else:
         return 'Invalid'
         
        
 
 def move(board,player_number):
-  '''This updates the provided board to the new board/game position and resturns the nested list of the new_board
-    
-    board: nested list (It is the current game position)
-            Eg. [[1/sqrt(2),'121221112'],[1/sqrt(2),'111221212']]
-    player_number: char (It is the number/char of the player)'''
-  
+    """"This updates the provided current board to a new board/game position
+        board: nested list (current board position)
+                Eg. [[1/sqrt(2),'121221112'],[1/sqrt(2),'111221212']]
+        player_number: char """
     new_board = []
     for i,j in board:
         zeros = [k for k in range(len(j)) if j.startswith('0', k)]
@@ -74,10 +78,11 @@ def move(board,player_number):
             new_board.extend(current_board)
         else:
             new_board = board 
-    
-    #Combining same boards
+
+    #Combining Same Boards
     flat = functools.reduce(operator.iconcat, new_board, [])
     duplicate_items = ([item for item, count in Counter(flat).items() if (count > 1 and type(item)==str)])
+    
     concatenated_list = []
     all_indexes = []
     for i in duplicate_items:
@@ -91,23 +96,24 @@ def move(board,player_number):
             total_amp += new_board[k][0]
         combined_ele = [total_amp,i]
         concatenated_list.append(combined_ele)
+
     for ele in sorted(all_indexes, reverse = True): 
-            del new_board[ele]
+            del new_board[ele]   
     new_board.extend(concatenated_list)
     
-    #Removing boards with amplitude 0
+    #Eliminating boards with no amplitude 
     unwanted = []
     for i in new_board:
         if(i[0]==False):
             unwanted.append(new_board.index(i))
     for ele in sorted(unwanted, reverse = True): 
         del new_board[ele]
-                    
+        
     return new_board
 
 
 def status(one_board,player_number):
-  ''' This function returns a 1 if that particular board is a winning board for the player, -1 if it is winning board for opponent and 0 if it's draw'''
+    """This returns 1 if the the player won this board, -1 is opponent won and 0 if it's a draw"""
     
     zeros = [k for k in range(len(one_board)) if one_board.startswith('0', k)]
     opponent_number = '2' if player_number=='1' else '1'
@@ -118,19 +124,16 @@ def status(one_board,player_number):
     combined = diags + rows + cols
     
     if(len(zeros)==0):
-        if(any(i.count(player_number)==3 for i in combined) and all(i.count(opponent_number)!=3 for i in combined)): #win condition
+        if(any(i.count(player_number)==3 for i in combined) and all(i.count(opponent_number)!=3 for i in combined)):
             return 1
-        elif (any(i.count(opponent_number)==3 for i in combined) and all(i.count(player_number)!=3 for i in combined)): #lose condition
+        elif (any(i.count(opponent_number)==3 for i in combined) and all(i.count(player_number)!=3 for i in combined)):
             return -1
-        else:                                                                                                           #draw
+        else:
             return 0
 
 def reward(board, player_number):
-  '''This returns the reward of the player entered'''
-  
     sum_rewards = 0
     for i,j in board:
-        #sum_rewards += round((abs(i**2))*status(j,player_number)*100,2)
         result = status(j,player_number)
         if(result==1):
             sum_rewards += (abs(i**2))*status(j,player_number)*100
@@ -140,8 +143,6 @@ def reward(board, player_number):
     
            
 def play():
-  '''This function plays the complete game and returns the rewards for both the players'''
-  
     turn = 1
     board = initial_board()
     p1 = '1'
@@ -151,16 +152,25 @@ def play():
             board = move(board,p1)
         elif(turn%2 == 0):
             board = board = move(board,p2)
-        print('Board after move',turn,':',board,'\n')
+        string =''
+        for i in board:
+            
+            if (board.index(i)==(len(board)-1)):           
+                string = string + '('+str(round(i[0].real,2)) + '+' +  str(round(i[0].imag,2)) + 'i'+')' + '|'+ i[1] + '>' 
+            else:
+                string = string + '('+str(round(i[0].real,2)) + '+' +  str(round(i[0].imag,2)) + 'i'+')' + '|'+ i[1] + '>'+ ' + '
+        print('Board after move',turn,':\n',string,'\n')
         turn+=1
     reward1 = reward(board,p1)
     reward2 = reward(board,p2)
     print('The final reward for player 1 is',reward1,'and for player 2 is',reward2)
-    return reward1, reward2
                 
+
+
 
 
 play()
 
-      
+ 
+  
 
