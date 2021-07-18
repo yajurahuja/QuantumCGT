81535ef4862e0f7745653bd88b1a8bbb18e44fa6
Yajur Ahuja <yajur16121@iiitd.ac.in>
2021-07-18 18:10:15 +0530

    ove function updated

--- TiQ-TaQ-Toe/RL/Classical_vs_Quantum_RL.py
+++ TiQ-TaQ-Toe/RL/Classical_vs_Quantum_RL.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import sparse
 
 
 import random
@@ -76,6 +77,7 @@ class Game:
 			return 0	
 
 	def play(self, rounds = 100):
+	""" The user enters the number of rounds and the game is played than many number of times"""
 		for i in range(rounds):
 			if i % 100 == 0:
 				print("Rounds " + str(rounds))
@@ -89,8 +91,8 @@ class Game:
 
 				for board in self.gameboards:
 					positions.append(self.availablePositions(board[1], player))
-				action = player.chooseAction(positions, self.board, (self.turn))
-				self.updateState(action)
+				actions, amps = player.chooseAction(positions, self.board, (self.turn))
+				self.move(actions, amps)
 				board_hash = self.getHash()
 				player1.addState(board_hash)
 				if(self.turn == 9)
@@ -100,7 +102,67 @@ class Game:
 			self.p1.reset()
 			self.p2.reset()
 			self.reset()
-			self.turn = 3 - self.turn
+			self.turn = 0
+
+	def move(self, actions, amps):
+	new_board = []
+	for n in range(len(self.gameboards)):
+		i = self.gameboards[n][0]
+		j = self.gameboards[n][1]
+        zeros = [k for k in range(len(j)) if j.startswith('0', k)]
+        if (len(zeros)>= 2):
+            amp, chosen_zeros = amps[n], actions[n]
+            new_amplitudes = [round((element*i).real,2)+round((element*i).imag,2)*1j for element in amp]
+            new_positions = []
+            for index in chosen_zeros:
+                new_positions.append(j[:index] + player_number + j[index + 1:])
+            current_board = [list(a) for a in zip(new_amplitudes, new_positions)]
+            new_board.extend(current_board)
+            #print('Initital:',i,j,'\nRandom Apms: ', amp, '\nRandom Positions: ',chosen_zeros,'\nNew Apms: ',new_amplitudes, '\nNew Positions: ',new_positions, '\nCurrent Board: ',current_board,'\n\n\n')
+        elif (len(zeros)==1):
+            for index in zeros:
+                new_positions = j[:index] + player_number + j[index + 1:]
+            current_board = [[i, new_positions]]
+            new_board.extend(current_board)
+        else:
+            new_board = board 
+    flat = functools.reduce(operator.iconcat, new_board, [])
+    duplicate_items = ([item for item, count in Counter(flat).items() if (count > 1 and type(item)==str)])
+    
+    concatenated_list = []
+    all_indexes = []
+    for i in duplicate_items:
+        indexes = []
+        for j in new_board:
+            if (i==j[1]):
+                indexes.append(new_board.index(j))
+                all_indexes.append(new_board.index(j))
+        total_amp = 0
+        for k in indexes:
+            total_amp += new_board[k][0]
+        combined_ele = [total_amp,i]
+        concatenated_list.append(combined_ele)
+
+    for ele in sorted(all_indexes, reverse = True): 
+            del new_board[ele]   
+    new_board.extend(concatenated_list)
+    
+    #Eliminating boards with no amplitude 
+    unwanted = []
+    for i in new_board:
+        if(i[0]==False):
+            unwanted.append(new_board.index(i))
+    for ele in sorted(unwanted, reverse = True): 
+        del new_board[ele]
+    
+    #Re-normalizing the board
+    a,b = map(list, zip(*new_board))
+    a = np.array(a)
+    a = list(a/np.linalg.norm(a))
+
+    new_board = [list(i) for i in zip(a,b)]
+        
+    return new_board
 
 
 	def getHash(self, gameboard):
@@ -139,20 +201,24 @@ class player:
 		self.state_values = {} # state -> value
 
 	def chooseAction(self, positions, current_board, turn):
-		fullamps = [1, -1 , 1j, -1j,]
+		fullamps = [1, -1 , 1j, -1j]
 		halfamps = [1/np.sqrt(2), -1/np.sqrt(2), 1j/np.sqrt(2), -1j/np.sqrt(2)]
+		totalamps = [1, -1 , 1j, -1j, 1/np.sqrt(2), -1/np.sqrt(2), 1j/np.sqrt(2), -1j/np.sqrt(2)]
 		if np.random.uniform(0, 1) <= self.exp_rate:
-			idx = [np.random.choice(len(position), size = (np.random.choice(2) + 1)) for position in a]
+			idx = [np.random.choice(len(position), size = 2) for position in a]
 			actions = [np.array(positions[i])[idx[i]] for i in range(len(positions))]
 			amps = []
 			for action in actions:
-				if size(action) == 1:
-					amp = np.random.choice(fullamps)
-					amps.append([amp])
-				else:
-					amp1 = np.random.choice(halfamps)
-					amp2 = np.random.choice(halfamps)
+					amp1 = np.random.choice(totalamps)
+					if amp1 in fullamps:
+						amp2 = 0
+					else:	
+						amp2 = np.random.choice(halfamps)
 					amps.append([amp1, amp2])
+		else: 
+
+
+		return actions, amps
 
 
 	def chooseCAction2(self, positions, current_board, turn):
